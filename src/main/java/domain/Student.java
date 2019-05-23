486b805414fa4bdde338bc578f15c9f41dc92473
2529716798 <2529716798@qq.com>
2019-05-23 16:09:31 +0800

    init

---
+++ src/main/java/domain/Student.java
@@ -0,0 +1,20 @@
+package domain;
+
+/**
+ * @Author: py
+ * @Date: 2019-05-23 16:06
+ * @Version 1.0
+ */
+public class Student {
+    private int id;
+
+    private String name;
+
+    private int age;
+
+    private int acYear;
+
+    private int graYear;
+
+
+}
